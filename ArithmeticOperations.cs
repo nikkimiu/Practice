d9bb14763a840ca61d28a19de0b87ed3ad2e2ddc
Nikki <shaoxinyi105@hotmail.com>
2018-03-04 15:01:37 +1300

    Wrote a program to add, divide, subtract and multiply

---
+++ ArithmeticOperations.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nikki
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter 2 numbers");
+            String firstNumber = Console.ReadLine();
+            int first = int.Parse(firstNumber);
+            String secondNumber = Console.ReadLine();
+            int second = int.Parse(secondNumber);
+            int sum = first + second;
+            Console.WriteLine(sum);
+            int difference = first - second;
+            Console.WriteLine(difference);
+            int product = first * second;
+            Console.WriteLine(product);
+            int divide = first / second;
+            Console.WriteLine(divide);
+
+
+        }
+    }
+}
