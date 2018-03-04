784aa9517d8c766316f41a11a152eff705543299
Nikki <shaoxinyi105@hotmail.com>
2018-03-04 14:33:14 +1300

    a Program to add two numbers

---
+++ SumOfTwoNumbers.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nikki
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write two Numbers");
+            int first = 0;
+            int second = 0;
+            String firstNumber = Console.ReadLine();
+            first = int.Parse(firstNumber);
+            String secondNumber = Console.ReadLine();
+            second = int.Parse(secondNumber);
+            int third = 0;
+            third = first + second;
+            Console.WriteLine("The sum of " + firstNumber + " and " + secondNumber + " is " + third);
+        }
+    }
+}
