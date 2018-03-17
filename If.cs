bf7f18b107b235aa1df722f75528b0b5c00aaa6b
Nikki <shaoxinyi105@hotmail.com>
2018-03-18 12:15:04 +1300

    I wrote a program with "if"

---
+++ If.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nikki
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a Name ");
+            String name = Console.ReadLine();
+            if (name == "Andy" || name == "Sherry")
+            {
+                Console.WriteLine("Your age is 27");
+            }
+            if (name == "Cherry")
+            {
+                Console.WriteLine("Your age is 32");
+            }
+        }
+    }
+}
