47b79bbc2ecb4fcc95b88ba8096cd4c3ecc77556
Nikki <shaoxinyi105@hotmail.com>
2018-03-18 14:42:47 +1300

    I wrote a application inputs one number consisting of five digits from the user, separates the number into its individual digits and prints the digits separated from one another

--- Division&ModulusOperations.cs
+++ Division&ModulusOperations.cs
@@ -16,10 +16,20 @@ namespace Nikki
             int user = 0;
             String userNumber = Console.ReadLine();
             user = int.Parse(userNumber);
-            int firstNumber = user / 10;
+            int firstNumber = user / 10000;
             Console.WriteLine(firstNumber);
+            int secondNumber = user / 1000 % 10;
+            Console.WriteLine(secondNumber);
+            int thirdNumber = user / 100 % 100 % 10;
+            Console.WriteLine(thirdNumber);
+            int fourthNumber = user % 100/10; 
+            Console.WriteLine(fourthNumber);
+            int lastNumber = user % 10;
+            Console.WriteLine(lastNumber);
 
-            
+
+
+
         }
     }
 }
