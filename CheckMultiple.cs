05d97b0406b3e7ee35c266f172d287f28d897ed4
Nikki <shaoxinyi105@hotmail.com>
2018-03-18 13:04:28 +1300

    I wrote a application to check multiple

---
+++ CheckMultiple.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nikki
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write two numbers");
+            int first = 0;
+            int second = 0;
+            String firstNumber = Console.ReadLine();
+            first = int.Parse(firstNumber);
+            String secondNumber = Console.ReadLine();
+            second = int.Parse(secondNumber);
+            int remainder = first % second;
+            if (remainder == 0)
+            {
+                Console.WriteLine("The first number is a multiple of the second ");
+            }
+            if (remainder != 0)
+            {
+                Console.WriteLine("The first number is not a multiple of the second");
+            }
+      
+
+
+        }
+    }
+}
